764ea41f5c3394466644e9744a2e9ace5efa1e09
Kulik <panasova97@gmail.com>
2021-02-01 21:59:10 +0300

    allure

--- src/test/java/org/example/StartPage.java
+++ src/test/java/org/example/StartPage.java
@@ -1,13 +1,11 @@
 package org.example;
 
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
